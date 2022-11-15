45657df9277cc5dfa5c790eb8509b0852fcc2225
xrosilloprofe <xv.rosillo@edu.gva.es>
2022-11-15 19:59:56 +0100

    version 0.1

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
         int[][] grades = new int[2][20];
         fillArray(grades);
         show(grades);
-
         //primero debemos ordenar la matriz
+        quickSort(grades, 0, grades[1].length-1);
+        show(grades);
 
         //luego podemos ordenarla
-
+        System.out.println("encontrado: " + busquedaBinaria(grades, 3));
 
     }
 
@@ -29,17 +30,17 @@ public class Main {
         }
     }
 
-    public static boolean busquedaBinaria(int[] array, int element) {
+    public static boolean busquedaBinaria(int[][] array, int element) {
         boolean encontrado = false;
         int primero = 0;
-        int ultimo=array.length-1;
+        int ultimo=array[1].length-1;
         int medio = (primero + ultimo)/2;
 
         while( primero <= ultimo && !encontrado){
-            if (array[medio] == element){
+            if (array[1][medio] == element){
                 encontrado = true;
                 //si el número que está en el medio es menor que el elemento a buscar, buscamos en la primera parte del array
-            }else if(array[medio] < element ){
+            }else if(array[1][medio] < element ){
                 primero = medio + 1;
             }else{
                 ultimo = medio - 1;
@@ -51,17 +52,17 @@ public class Main {
 
     }
 
-    public static void quickSort(int[] v, int left, int right) {
+    public static void quickSort(int[][] v, int left, int right) {
 
-        int pivot = v[(left + right)/2];
+        int pivot = v[1][(left + right)/2];
         int i = left;
         int d = right;
 
         do {
-            while (v[right] > pivot) {
+            while (v[1][right] > pivot) {
                 right--;
             }
-            while (v[left] < pivot) {
+            while (v[1][left] < pivot) {
                 left++;
             }
             if (left <= right) {
@@ -79,10 +80,12 @@ public class Main {
 
     }
 
-    private static void swap(int[] v, int i, int j) {
-        int aux = v[i];
-        v[i] = v[j];
-        v[j] = aux;
+    private static void swap(int[][] v, int i, int j) {
+        for (int k = 0; k < v.length; k++) {
+            int aux = v[k][i];
+            v[k][i] = v[k][j];
+            v[k][j] = aux;
+        }
     }
 
 }
